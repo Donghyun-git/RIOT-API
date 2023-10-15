1ad534912cb2721d03519ffa63656fe0428a9917
donghyun-git <donghyun1113@hanmail.net>
2023-10-15 19:49:13 +0900

    feat: routes setting

--- packages/client/src/routes/Router.tsx
+++ packages/client/src/routes/Router.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import { PATH } from '../constants/paths';
 
-import { MainPage } from '../pages/MainPage';
+import { MainPage } from '@pages/MainPage';
+import { LoginPage } from '@pages/LoginPage';
 
 function Router() {
   return (
     <Routes>
       <Route index path={PATH.root} element={<MainPage />} />
+      <Route path={PATH.login} element={<LoginPage />} />
+      <Route path={PATH.signup} />
+      <Route path={PATH.search} />
+      <Route path={PATH.community} />
+      <Route path={PATH.match} />
     </Routes>
   );
 }
