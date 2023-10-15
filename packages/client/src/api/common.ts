f051d33674d11f5fb7c275b8b114ee86340a5d13
donghyun-git <donghyun1113@hanmail.net>
2023-10-15 19:46:33 +0900

    chore: baseurl 세팅

--- packages/client/src/api/common.ts
+++ packages/client/src/api/common.ts
@@ -17,34 +17,34 @@ export const customAuthFetch = axios.create({
  * 세션이 만료되어 어플리케이션이 새로고침 될거기 때문에
  * 이후에 들어오는 response들은 모두 세션 만료를 무시한다.
  */
-let isExitApplication = false;
+// let isExitApplication = false;
 
-customAuthFetch.interceptors.request.use((config) => {
-  if (isExitApplication) {
-    throw new axios.Cancel('Application Exit!');
-  }
+// customAuthFetch.interceptors.request.use((config) => {
+//   if (isExitApplication) {
+//     throw new axios.Cancel('Application Exit!');
+//   }
 
-  // const { accesstoken } = useUserStore.getState();
+//   // const { accesstoken } = useUserStore.getState();
 
-  // config.headers.set('accesstoken', accesstoken, true);
+//   // config.headers.set('accesstoken', accesstoken, true);
 
-  return config;
-});
+//   return config;
+// });
 
-customAuthFetch.interceptors.response.use((res) => {
-  if (!isExitApplication && res.data?.result.status.startsWith('8')) {
-    alert(
-      '이용시간이 만료되었거나 다른 기기에서 로그인되어 자동로그아웃 되었습니다. 서비스를 계속 이용하시려면 다시 로그인 하시기 바랍니다!',
-    );
+// customAuthFetch.interceptors.response.use((res) => {
+//   if (!isExitApplication && res.data?.result.status.startsWith('8')) {
+//     alert(
+//       '이용시간이 만료되었거나 다른 기기에서 로그인되어 자동로그아웃 되었습니다. 서비스를 계속 이용하시려면 다시 로그인 하시기 바랍니다!',
+//     );
 
-    // useUserStore.persist.clearStorage();
+//     // useUserStore.persist.clearStorage();
 
-    location.href = PATH.root;
-    isExitApplication = true;
-    return res;
-  }
+//     location.href = PATH.root;
+//     isExitApplication = true;
+//     return res;
+//   }
 
-  redirectDocument(PATH.root);
+//   redirectDocument(PATH.root);
 
-  return res;
-});
+//   return res;
+// });
