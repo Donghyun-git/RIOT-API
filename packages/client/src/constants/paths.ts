541e4de501fcf690eedde5e2d5a5aa859f21bedb
donghyun-git <donghyun1113@hanmail.net>
2023-10-15 19:49:02 +0900

    chore: path constants

--- packages/client/src/constants/paths.ts
+++ packages/client/src/constants/paths.ts
@@ -1,5 +1,8 @@
 export const PATH = {
   root: '/' as const,
+  login: '/login' as const,
+  signup: '/signup' as const,
   search: '/search' as const,
-  community: '/communitiy' as const,
+  community: '/community' as const,
+  match: '/match' as const,
 };
